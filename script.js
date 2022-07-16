53e10d0508634592f9ccf896bc13d4af0dd0ff87
jor-eldevera <jorell.devera@gmail.com>
2022-07-15 18:32:18 -0700

    Change square color when hovered over

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const squaresContainer = document.querySelector(".squares-container");
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        // square.textContent = "s";
         squaresContainer.appendChild(square);
+
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.background = "black";
+        });
     }
 }
